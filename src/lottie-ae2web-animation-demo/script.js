942c02c4a55fd82496a15c69c7873392095130b5
roy <croyvroy@gmail.com>
2022-12-26 13:00:44 +0800

    complete lottie demo

---
+++ src/lottie-ae2web-animation-demo/script.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+
+    const cards = document.querySelectorAll('.card-item');
+    cards.forEach((card) => {
+        const bodymovinLayer = card.querySelector(".card-img");
+        
+        var anima = lottie.loadAnimation({
+            container: bodymovinLayer,
+            loop: false,
+            autoplay: false,
+            renderer: 'svg',
+            path: bodymovinLayer.dataset.path
+        });
+    
+        card.addEventListener("mouseenter", () => { anima.play() });
+        card.addEventListener("mouseleave", () => { anima.stop() });
+    });
+}
